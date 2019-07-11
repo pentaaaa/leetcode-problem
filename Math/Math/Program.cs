65f818d12692d9ccb850b8e9911ebf07791c58e2
Tom Pong <975037188@qq.com>
2019-07-11 15:34:05 +0800

    the first commit

---
+++ Math/Math/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        //给定一个整数，判断它是否是3的幂次方
+        public bool IsPowerOfThree(int n)
+        {
+            while (n >= 3)
+            {
+                if (n == 3)
+                    return true;
+                n /= 3;
+            }
+            return false;
+        }
+    }
+}
